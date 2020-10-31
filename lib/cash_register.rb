4c797120964a6946524939a768123c3be356dc43
Beka23 <beksultankaiypov@gmail.com>
2020-10-31 16:51:55 -0400

    commit tree b021f80eff1b533910183db9295dd8bd069424e5

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
+class CashRegister
 
+    attr_accessor :discount, :total
+
+    def initialize(discount = 0)
+        @total = 0
+        @discount = discount
+
+    end
+
+    def add_item
+    end
+
+   
+end
